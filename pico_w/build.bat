aa58e9ed944f180cd05ac3c4db132b7a0a1d5d38
Luan Daniel <luandanielmelo@gmail.com>
2023-10-05 19:26:02 -0300

    fix batch scripts folder generation

--- pico_w/build.bat
+++ pico_w/build.bat
@@ -4,13 +4,13 @@ set gen_dir="gen"
 set src_dir="src"
 set build_dir="build"
 
-if not exit %gen_dir% (mkdir %gen_dir%)
-if not exit %build_dir% (mkdir %build_dir%)
+if not exist %gen_dir% (mkdir %gen_dir%)
+if not exist %build_dir% (mkdir %build_dir%)
 
 if "%~1" == "--clear" (
   echo "[i] Cleaning build files..."
-  rd /S /Q "%gen_dir%"
-  rd /S /Q "%build_dir%"
+  rd /S /Q %gen_dir%
+  rd /S /Q %build_dir%
   exit /B
 )
 
