5c0aad0adf64733ed14d1a812ee262a0734a520e
Luan Daniel <luandanielmelo@gmail.com>
2023-10-05 19:15:36 -0300

    chore ident fix

--- pico_w/src/server/main.c
+++ pico_w/src/server/main.c
@@ -12,58 +12,58 @@
 #define PARITY    UART_PARITY_NONE
 
 int main() {
-    stdio_init_all();
+  stdio_init_all();
 
-    // Initialize the UART peripheral
-    uart_init(UART_ID, BAUD_RATE);
-    gpio_set_function(0, GPIO_FUNC_UART);
-    gpio_set_function(1, GPIO_FUNC_UART);
-    uart_set_hw_flow(UART_ID, false, false);
-    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+  // Initialize the UART peripheral
+  uart_init(UART_ID, BAUD_RATE);
+  gpio_set_function(0, GPIO_FUNC_UART);
+  gpio_set_function(1, GPIO_FUNC_UART);
+  uart_set_hw_flow(UART_ID, false, false);
+  uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
 
-    // Initialize the GPIO pin for the LED
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+  // Initialize the GPIO pin for the LED
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    // Initialize the UDP socket
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sock < 0) {
-        perror("socket");
-        exit(1);
+  // Initialize the UDP socket
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 0) {
+    perror("socket");
+    exit(1);
+  }
+
+  // Bind the socket to port 8888
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(8888);
+  addr.sin_addr.s_addr = INADDR_ANY;
+  if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    perror("bind");
+    exit(1);
+  }
+
+  // Receive data from the client and send a response
+  char buffer[1024];
+  struct sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(clientAddr);
+
+  while (1) {
+    int numBytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (numBytes < 0) {
+      perror("recvfrom");
+      exit(1);
     }
 
-    // Bind the socket to port 8888
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(8888);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        exit(1);
-    }
+    // Toggle the LED
+    gpio_xor_mask(1 << PICO_DEFAULT_LED_PIN);
 
-    // Receive data from the client and send a response
-    char buffer[1024];
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
+    // Send a response to the client
+    char* response = "Server Discovery Response: 192.168.1.100:8888";
+    sendto(sock, response, strlen(response), 0, (struct sockaddr*)&clientAddr, clientAddrLen);
+    printf("Received message from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+    printf("Response sent to %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+    printf("Message: %s\n", buffer);
+  }
 
-    while (1) {
-        int numBytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
-        if (numBytes < 0) {
-            perror("recvfrom");
-            exit(1);
-        }
-
-        // Toggle the LED
-        gpio_xor_mask(1 << PICO_DEFAULT_LED_PIN);
-
-        // Send a response to the client
-        char* response = "Server Discovery Response: 192.168.1.100:8888";
-        sendto(sock, response, strlen(response), 0, (struct sockaddr*)&clientAddr, clientAddrLen);
-        printf("Received message from %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-        printf("Response sent to %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-        printf("Message: %s\n", buffer);
-    }
-
-    return 0;
+  return 0;
 }
